f2ca907bd5dd85b225022ea7cda02607888490fe
Matteo Calvanico <matteo.calvanico@maggioli.it>
2024-11-27 17:36:18 +0100

    Added new projects

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -47,6 +47,30 @@ export class ProjectsService {
       technologies: [this.skillService.getSkillByName('Vue.js')!, this.skillService.getSkillByName('HTML5')!, this.skillService.getSkillByName('Saas')!, this.skillService.getSkillByName('Express.js')!, this.skillService.getSkillByName('Bootstrap')!, this.skillService.getSkillByName('TypeScript')!],
       image: 'https://github.com/MatteoCalvanico/GetEat/blob/main/frontend/public/logo.png?raw=true'
     },
+    {
+      name: 'ProtoCRPG',
+      description: 'A simple prototype for a CRPG in Godot 4.3, made like a template for other projects',
+      githubUrl: 'https://github.com/MatteoCalvanico/ProtoCRPG',
+      otherUrls: [],
+      technologies: [this.skillService.getSkillByName('Godot')!],
+      image: 'https://github.com/MatteoCalvanico/ProtoCRPG/blob/prototype/src/preview.png?raw=true'
+    },
+    {
+      name: 'DoomClone',
+      description: 'A simple C++ game similar to Doom and Wolfenstein 3D',
+      githubUrl: 'https://github.com/MatteoCalvanico/DoomClone?tab=readme-ov-file',
+      otherUrls: [],
+      technologies: [this.skillService.getSkillByName('C++')!],
+      image: 'https://github.com/MatteoCalvanico/DoomClone/blob/main/output/12-10-24.png?raw=true'
+    },
+    {
+      name: 'EmbeddedProject',
+      description: 'A simple IoT project with ESP32 and a Raspberry Pi',
+      githubUrl: 'https://github.com/MatteoCalvanico/EmbeddedProject',
+      otherUrls: [{url: 'https://wokwi.com/projects/389703246864781313', title: 'Wokwi Simulation'}],
+      technologies: [this.skillService.getSkillByName('Arduino')!, this.skillService.getSkillByName('C')!],
+      image: '../images/EmbeddedProject.png'
+    },
   ]
 
   getAllProjects(): Project[] {
