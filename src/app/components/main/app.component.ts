c1c9b1223aeed90d3b4ce34b014d889c74b24bde
Matteo Calvanico <matteo.calvanico@maggioli.it>
2024-11-27 13:58:46 +0100

    first experimentation with ngFor and ngIf to show projects

--- src/app/components/main/app.component.ts
+++ src/app/components/main/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//import { RouterOutlet } from '@angular/router';
+
+// Components
+import { PersonalInfoComponent } from '../personal-info/personal-info.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { SkillComponent } from '../skill/skill.component';
+import { ProjectComponent } from '../project/project.component';
+
+// Interfaces
+import { Project } from '../../interfaces/project';
+import { Skill } from '../../interfaces/skill';
+import { Experience } from '../../interfaces/experience';
+
+// Services
+import { ExperiencesService } from '../../services/experiences.service';
+import { ProjectsService } from '../../services/projects.service';
+import { SkillsService } from '../../services/skills.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, PersonalInfoComponent, ExperienceComponent, SkillComponent, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-portfolio';
+  projectsService: ProjectsService = inject(ProjectsService);
+  skillsService: SkillsService = inject(SkillsService);
+  experiencesService: ExperiencesService = inject(ExperiencesService);
+
+  projects: Project[] = [];
+  skills: Skill[] = [];
+  experiences: Experience[] = [];
+
+  constructor() {
+    this.projects = this.projectsService.getAllProjects();
+    this.skills = this.skillsService.getAllSkills();
+    /*this.experiences = this.experiencesService.getAllExperiences();*/
+  }
 }
--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Project } from '../../interfaces/project';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
-
+  @Input() project!: Project;
 }
