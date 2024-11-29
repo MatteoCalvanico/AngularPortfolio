4a47b1e2f76852f772b8f97f94cd86b5e4e4b379
Matteo Calvanico <matteo.calvanico@maggioli.it>
2024-11-29 14:41:32 +0100

    Updated skills loop  implementation

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -25,26 +25,48 @@ export class SkillComponent implements OnInit, AfterViewInit {
   }
 
   animateSkills(): void {
-    const container = document.querySelector('.skills-container');
+    const container = document.querySelector('.skills-container') as HTMLElement;
     const skillElements = document.querySelectorAll('.skill');
-    const totalWidth = Array.from(skillElements).reduce((acc, el) => acc + el.clientWidth + 20, 0); // 20 is the margin
-
+    const totalWidth = Array.from(skillElements).reduce((acc, el) => acc + (el as HTMLElement).clientWidth + 20, 0); // 20 is the margin
+  
     if (container) {
       // Duplicate the skills to create a seamless loop
       container.innerHTML += container.innerHTML;
-
+  
       anime({
         targets: '.skills-container',
         translateX: [0, -totalWidth],
-        duration: 500000,
+        duration: 1200000,
         easing: 'linear',
         loop: true,
         update: function(anim) {
           const firstSkill = container.querySelector('.skill');
           if (firstSkill && firstSkill.getBoundingClientRect().right < 0) {
             container.appendChild(firstSkill);
+            firstSkill.classList.remove('visible'); // Remove visible class
+            setTimeout(() => {
+              firstSkill.classList.add('visible'); // Re-add visible class after a short delay
+            }, 50);
             anim.restart();
           }
+  
+          // Check visibility of each skill and update opacity
+          document.querySelectorAll('.skill').forEach(skill => {
+            const rect = (skill as HTMLElement).getBoundingClientRect();
+            if (rect.left >= 0 && rect.right <= window.innerWidth) {
+              skill.classList.add('visible');
+            } else {
+              skill.classList.remove('visible');
+            }
+          });
+        }
+      });
+  
+      // Initially add visible class to all skills that are in view
+      document.querySelectorAll('.skill').forEach(skill => {
+        const rect = (skill as HTMLElement).getBoundingClientRect();
+        if (rect.left >= 0 && rect.right <= window.innerWidth) {
+          skill.classList.add('visible');
         }
       });
     }
