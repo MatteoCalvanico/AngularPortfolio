2953910d4c2eaa6e2f2cd67d6db36251f5686bd4
Matteo Calvanico <matteo.calvanico@maggioli.it>
2024-11-29 12:58:02 +0100

    Created Skills section, first experimentation with circolar movement

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -1,18 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import anime from 'animejs/lib/anime.es.js';
 
-/**
- * Component representing a skill.
- *
- * @selector app-skill
- * @templateUrl ./skill.component.html
- * @styleUrl ./skill.component.scss
- */
+import { SkillsService } from '../../services/skills.service';
+import { Skill } from '../../interfaces/skill';
+
 @Component({
   selector: 'app-skill',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill.component.html',
-  styleUrl: './skill.component.scss'
+  styleUrls: ['./skill.component.scss']
 })
-export class SkillComponent {
+export class SkillComponent implements OnInit, AfterViewInit {
+  skills: Skill[] = [];
 
+  constructor(private skillsService: SkillsService) {}
+
+  ngOnInit(): void {
+    this.skills = this.skillsService.getAllSkills();
+  }
+
+  ngAfterViewInit(): void {
+    this.animateSkills();
+  }
+
+  animateSkills(): void {
+    const container = document.querySelector('.skills-container');
+    const skillElements = document.querySelectorAll('.skill');
+    const totalWidth = Array.from(skillElements).reduce((acc, el) => acc + el.clientWidth + 20, 0); // 20 is the margin
+
+    if (container) {
+      // Duplicate the skills to create a seamless loop
+      container.innerHTML += container.innerHTML;
+
+      anime({
+        targets: '.skills-container',
+        translateX: [0, -totalWidth],
+        duration: 500000,
+        easing: 'linear',
+        loop: true,
+        update: function(anim) {
+          const firstSkill = container.querySelector('.skill');
+          if (firstSkill && firstSkill.getBoundingClientRect().right < 0) {
+            container.appendChild(firstSkill);
+            anim.restart();
+          }
+        }
+      });
+    }
+  }
 }
