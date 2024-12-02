aa86fce2d02e3b2437024856e8ca84c2eae47308
Matteo Calvanico <matteo.calvanico@maggioli.it>
2024-12-02 12:53:07 +0100

    Added scale animation on contacts over

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
 
+  ngOnInit() {
+    this.addHoverAnimation();
+  }
+
+  /**
+   * Adds hover animation to all images in the contact links.
+   */
+  addHoverAnimation() {
+    const images = document.querySelectorAll('.contact-links img');
+    images.forEach((img) => {
+      img.addEventListener('mouseenter', () => {
+        anime({
+          targets: img,
+          scale: 1.2,
+          duration: 300,
+          easing: 'easeInOutQuad'
+        });
+      });
+
+      img.addEventListener('mouseleave', () => {
+        anime({
+          targets: img,
+          scale: 1,
+          duration: 300,
+          easing: 'easeInOutQuad'
+        });
+      });
+    });
+  }
 }
