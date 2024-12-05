0cc886e80baae57d73abfc86f15974af14bece08
Matteo Calvanico <matteo.calvanico@maggioli.it>
2024-12-05 12:30:46 +0100

    Modified RepoGetter to use ProjectScaraper

--- python_scripts/RepoGetter.py
+++ python_scripts/RepoGetter.py
@@ -1,9 +1,12 @@
 ## This script gets the repositories of a GitHub user and saves them in a JSON file.
 #  First experiment for automate the process of updating the projects section of my portfolio.
 
+import os
 import requests             # Import the requests library to make HTTP requests
 import json                 # Import the json library to parse JSON responses
 from github import Github   # Import the Github API
+
+from ProjectScraper import ProjectScraper # Import the ProjectScraper class
 
 
 API_URL = 'https://api.github.com'
@@ -21,19 +24,43 @@ def get_user_repos(username):
 
 
 
-
-
 if __name__ == '__main__':
-    username = 'MatteoCalvanico'
-    filter = ['MatteoCalvanico/MatteoCalvanico', 'MatteoCalvanico/CV'] # Repositories to exclude
 
-    repos = get_user_repos(username)
+    # Initialize the ProjectScraper objects and get the repositories names
+    psUni = ProjectScraper('https://github.com/stars/MatteoCalvanico/lists/uni-projects')
+    psPersonal = ProjectScraper('https://github.com/stars/MatteoCalvanico/lists/personal-projects')
+    repo_names = psUni.get_repo_names()
+    repo_names += psPersonal.get_repo_names()
+
+    print("Repositories names: ")
+    for name in repo_names:
+        print("- ", name)
+    print("-"*100)
+    print()
+
+
+    # Get the repositories of the user
+    username = 'MatteoCalvanico'
+    print("Searching repos' info...")
+    try:
+        repos = get_user_repos(username)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        repos = []
+    print("Done!")
+    print("-"*100)
+    print()
+
+
+    # Create the output directory if it doesn't exist
+    os.makedirs('output', exist_ok=True)
     
-    print("Saving info in repo.json...")
+    # Save the repositories in a JSON file
+    print("Saving repos' info in output/repo.json...")
     repo_list = []
     for repo in repos:
-        if not repo.archived and repo.full_name not in filter: # Skip archived repositories and GitHub config repo
-            language = repo.get_languages().keys() # Get all the languages used in the repository
+        if repo.full_name in repo_names: # Take only what I want
+            language = list(repo.get_languages().keys()) # Get all the languages used in the repository
             repo_info = {
                 "full_name": repo.full_name,
                 "description": repo.description,
@@ -48,10 +75,14 @@ if __name__ == '__main__':
     print("Done!")
     print("-"*100)
     print()
+    print()
+    print()
     
+
+    # Print the information about the repositories
     print('Printing information about the repositories...')
     for repo in repos:
-        if not repo.archived and repo.full_name not in filter: # Skip archived repositories and GitHub config repo
+        if repo.full_name in repo_names: # Take only what I want
             print("Full name: " + repo.full_name)
             if repo.description:
                 print("Description: " + repo.description)
