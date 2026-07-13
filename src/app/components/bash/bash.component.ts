fe1725963ec884d6318113d8a1a8470349c45e21
MatteoCalvanico <matteo.calvanico@dma.it>
2026-07-13 16:40:17 +0200

    feat: refresh portfolio visual language

--- src/app/components/bash/bash.component.ts
+++ src/app/components/bash/bash.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import Typed from 'typed.js';
 
 /**
  * Component representing a bash-like interface.
- * 
+ *
  * @selector app-bash
  * @templateUrl ./bash.component.html
  * @styleUrls ./bash.component.scss
@@ -11,23 +11,31 @@ import Typed from 'typed.js';
 @Component({
   selector: 'app-bash',
   templateUrl: './bash.component.html',
-  styleUrls: ['./bash.component.scss']
+  styleUrls: ['./bash.component.scss'],
 })
-export class BashComponent implements OnInit {
+export class BashComponent implements OnInit, OnDestroy {
+  private typed?: Typed;
 
   /**
    * Initializes the component.
    */
   ngOnInit() {
     const options = {
-      strings: ["", "Welcome, I'm a Software Developer", "Welcome, I'm a Game Developer"],
+      strings: [
+        '',
+        "Welcome, I'm a Full-Stack Software Engineer",
+        "Welcome, I'm a Game Developer",
+      ],
       typeSpeed: 50,
       backSpeed: 0,
       smartBackspace: true,
-      loop: true
+      loop: true,
     };
 
-    const typed = new Typed('.typed-element', options);
+    this.typed = new Typed('.typed-element', options);
   }
-  
+
+  ngOnDestroy(): void {
+    this.typed?.destroy();
+  }
 }
--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,40 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import anime from 'animejs/lib/anime.es.js';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent implements OnInit {
-
-  ngOnInit() {
-    this.addHoverAnimation();
-  }
-
-  /**
-   * Adds hover animation to all images in the contact links.
-   */
-  addHoverAnimation() {
-    const images = document.querySelectorAll('.contact-links img');
-    images.forEach((img) => {
-      img.addEventListener('mouseenter', () => {
-        anime({
-          targets: img,
-          scale: 1.2,
-          duration: 300,
-          easing: 'easeInOutQuad'
-        });
-      });
-
-      img.addEventListener('mouseleave', () => {
-        anime({
-          targets: img,
-          scale: 1,
-          duration: 300,
-          easing: 'easeInOutQuad'
-        });
-      });
-    });
-  }
-}
+export class ContactsComponent {}
